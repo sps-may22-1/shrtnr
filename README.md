89aeac78d8d4e9f233562ec423b89a8d924f3677
Maks Atygaev <atygaev.mi@gmail.com>
2022-05-19 14:55:15 +0700

    Init project

---
+++ README.md
@@ -0,0 +1,12 @@
+# Shrtnr
+
+### Build and run
+
+MacOs or Linux
+```
+./gradlew clean bootRun
+```
+or Windows
+```
+gradlew.bat clean bootRun
+```
---
+++ src/main/java/com/example/shrtnr/ShrtnrApplication.java
@@ -0,0 +1,12 @@
+package com.example.shrtnr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShrtnrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShrtnrApplication.class, args);
+    }
+}
